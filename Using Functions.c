f1b5e64fef91865a3213faad43ce6492b02908b9
Grooci <dannyhumble361@gmail.com>
2025-01-08 13:58:56 -0800

    commit 1
    Question 1
    Finding total sale price. Declared original price and discount percentage. Also declared sale price with its equation and printed.
    
    Question 2
    Finding salary after tax. Declared original salary and tax rate. Also declared salary after tax with its equation and printed.

---
+++ Using Functions.c
@@ -0,0 +1,31 @@
+/*#include <stdio.h>
+
+void SalePrice() {
+    double originalPrice = 200000;
+    double discountPercentage = 0.10;
+    double salePrice = originalPrice - (originalPrice * discountPercentage);
+
+    printf("The total sale price is UGX %.2lf\n", salePrice);
+}
+
+int main() {
+    SalePrice();
+    return 0;
+}*/
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+void SalaryAfterTax(){
+    double OriginalSalary = 5000000;
+    double TaxRate = 0.15;
+    double SalaryAfterTax = OriginalSalary - (OriginalSalary * TaxRate);
+
+    printf("The Salary After Tax is UGX %.2lf\n", SalaryAfterTax);
+}
+
+int main() {
+    SalaryAfterTax();
+    return 0;
+}*/
+
